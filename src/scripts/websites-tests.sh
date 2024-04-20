b7d22d23e7c10b4675454c3e83b0e645be285415
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-04-20 17:02:31 -0400

    removed some options from script to avoid creating unneeeded folders

--- src/scripts/websites-tests.sh
+++ src/scripts/websites-tests.sh
@@ -7,20 +7,27 @@ WEBSITES=(
 
 # Function to check the HTTP status code of a URL
 check_url() {
-	response=$(curl -o /dev/null --silent --head --write-out '%{http_code}' "$1")
-	if [ "$response" != "200" ]; then
-		echo "$1: Problem found - HTTP status $response"
-	else
-		echo "$1: No problems detected."
-	fi
+	local url=$1
+	response=$(curl -o /dev/null --silent --head --write-out '%{http_code}' "$url")
+	echo -n "$url -> $response | "
 }
 
 # Function to find broken links on a website using wget
 check_links() {
-	echo "Checking for broken links on $1"
-	wget --spider -o log.txt -r -l 1 $1
-	if [ -f log.txt ]; then
-		grep -B 2 '404 Not Found' log.txt
+	local url=$1
+	local logfile="log-$RANDOM.txt" # Creates a unique log file name
+
+	# Perform the wget operation
+	wget --spider -o $logfile -r -l 1 -nd $url # Added -nd to prevent directory creation
+
+	# Check for 404 Not Found errors in the log file and count them
+	if [ -f $logfile ]; then
+		broken_links=$(grep -o '404 Not Found' $logfile | wc -l)
+		echo "Broken URLs: $broken_links found."
+		if [ "$broken_links" -ne 0 ]; then
+			grep -B 2 '404 Not Found' $logfile | grep 'URL:' | awk -F' ' '{print $3}' | sort | uniq
+		fi
+		rm $logfile
 	else
 		echo "Log file not created. Check wget command."
 	fi
@@ -28,7 +35,7 @@ check_links() {
 
 # Main loop to check each website
 for website in "${WEBSITES[@]}"; do
-	echo "Testing website: $website"
+	echo -n "Testing website: "
 	check_url $website
 	check_links $website
 done
